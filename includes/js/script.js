6bce751ab7f721681efb100a857b93d0aee56ae9
Sandy <singhvarpreet18@gmail.com>
2016-06-28 13:22:37 +0530

    Test_load_10

--- includes/js/script.js
+++ includes/js/script.js
@@ -8,3 +8,12 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+
+$(function()
+{
+  $('#alertMe').click(function(e)
+  {
+    e.preventDefault();
+    $('#successAlert').slideDown();
+  });
+});
