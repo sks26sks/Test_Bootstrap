a71fd9b3cfac05e0a1d897d289daad6cdc760226
Sandy <singhvarpreet18@gmail.com>
2016-06-28 16:58:59 +0530

    Test_load_12

--- includes/js/script.js
+++ includes/js/script.js
@@ -16,4 +16,13 @@ $(function()
     e.preventDefault();
     $('#successAlert').slideDown();
   });
+
+  $('a.pop').click(function(e)
+  {
+    e.preventDefault();
+  });
+
+  $('a.pop').popover();
+
+  $('[rel="tooltip"]').tooltip();
 });
